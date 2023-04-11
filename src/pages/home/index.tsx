31dc6193f15f9ff81df3c35b4b7e3d6fa19edf7f
Pedro Correia <correia.pedro@outlook.com>
2023-04-11 10:05:01 -0300

    :lipstick: Estrutura visual home

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,31 @@
+import { Heading, Text } from '@ignite-ui/react'
+import Image from 'next/image'
+
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl" as="h1">
+          Agendamento descomplicado
+        </Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          priority
+          height={400}
+          quality={100}
+          alt="Calendário simbolizando aplicação em funcionamento"
+        />
+      </Preview>
+    </Container>
+  )
+}
